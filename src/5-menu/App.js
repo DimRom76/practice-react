3cd4f0595366dfb45f117066090027236fd930a8
Dmitriy Kotulsky <66743608+DimRom76@users.noreply.github.com>
2020-11-09 23:20:34 +0200

    menu

--- src/5-menu/App.js
+++ src/5-menu/App.js
@@ -5,9 +5,20 @@ import items from "./data";
 
 import "./App.css";
 
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <main>
@@ -17,8 +28,8 @@ function App() {
           <div className="underline"></div>
         </div>
       </section>
-      <Categories />
-      <Menu />
+      <Categories categories={categories} filterItems={filterItems} />
+      <Menu items={menuItems} />
     </main>
   );
 }
