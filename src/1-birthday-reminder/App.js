f1d221b0e6ad275b70e6af2ccdfa2a838b976909
Dmitriy Kotulsky <66743608+DimRom76@users.noreply.github.com>
2020-11-13 23:56:01 +0200

    slider

--- src/1-birthday-reminder/App.js
+++ src/1-birthday-reminder/App.js
@@ -7,7 +7,7 @@ import "./App.css";
 function App() {
   const [people, setPeople] = useState(data);
   return (
-    <main class="main-reminder">
+    <main className="main-reminder">
       <section className="container-reminder">
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
--- src/5-menu/Menu.js
+++ src/5-menu/Menu.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Menu = ({ items }) => {
   return (
-    <div className="section-center">
+    <div className="section-center-menu">
       {items.map((menuItem) => {
         const { id, title, img, desc, price } = menuItem;
         return (
